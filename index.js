a6fd04194cf8e819d779cd235f9a71f543e74336
BrendaBii <bii.brenda.bcb@gmail.com>
2023-06-13 14:10:51 +0300

    Completed assignment.

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+let distance;
+function scuberGreetingForFeet(distance){
   // Write your code here!
-}
+  if(distance <= 400){
+    let response =  `This one is on me!`;
+    return response;
+  }
+  else if (distance > 2000 && distance <= 2500){
+    return `I will gladly take your thirty bucks.`
+  }
+  else if (distance > 2500){
+    return `No can do.`
+  }
+};
+scuberGreetingForFeet();
 
-function ternaryCheckCity(){
+let city;
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
+ternaryCheckCity();
 
-function switchOnCharmFromTip(){
+let tip;
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let response;
+  switch(tip){
+    case "generous" : response = "Thank you so much.";
+    break;
+    case "not as generous" : response = "Thank you.";
+    break;
+    default : response = "Bye.";
+  }
+  return response;
 }
+switchOnCharmFromTip();
